00f7c0acaad382f3e71adc482443a50202c55e6d
wired-filipino-owl <rob.galeoto@oit.edu>
2024-06-13 16:40:25 -0700

    Copy final code used to outside of demo folder

---
+++ src/final_jun_24/i2c_test.cpp
@@ -0,0 +1,314 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+#include <src/per/i2c.h>
+#include <cstdio>
+#include "mpr121.h"
+
+#define DEBUG_PRINT false
+#define ENV_ATK_TIME 0.05F
+#define ENV_DECAY_TIME 0.4F
+
+// Use the daisy namespace to prevent having to type
+// daisy:: before all libdaisy functions
+using namespace daisy;
+using namespace daisysp;
+
+static DaisySeed hw;
+
+Oscillator c5, d5, e5, f5, g5, a5, b5, c6;
+Oscillator lfo;
+AdEnv c5_env, d5_env, e5_env, f5_env, g5_env, a5_env, b5_env, c6_env;
+
+uint8_t touch_lo, touch_hi = 0;
+
+float sig, vol;
+float c5_out, d5_out, e5_out, f5_out, g5_out, a5_out, b5_out, c6_out;
+float lfo_out;
+
+float hall_val = 0.0;
+
+void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
+                   AudioHandle::InterleavingOutputBuffer out,
+                   size_t                                size)
+{
+    //hw.Print(".\n");
+    vol = hw.adc.GetFloat(1);
+    //magic numbers are from the "at rest" value of the Hall sensor
+    //and the intended midpoint frequency for the LFO
+    hall_val = hw.adc.GetFloat(0);
+    //hall_val = hall_val - 0.3;
+    lfo.SetFreq(hall_val * 2.357 * 1.2 * 7.0);
+    //lfo.SetFreq(hall_val * hall_val  * 7.0);
+
+    if (touch_lo & 0x02)
+    {
+        f5_env.Trigger();
+    }
+    else if (touch_lo & 0x04)
+    {
+        e5_env.Trigger();
+    }
+    else if (touch_lo & 0x08)
+    {
+        d5_env.Trigger();
+    }
+    else if (touch_lo & 0x10)
+    {
+        c5_env.Trigger();
+    }
+    else if (touch_lo & 0x80)
+    {
+        g5_env.Trigger();
+    }
+    else if (touch_hi & 0x01)
+    {
+        a5_env.Trigger();
+    }
+    else if (touch_hi & 0x02)
+    {
+        b5_env.Trigger();
+    }
+    else if (touch_hi & 0x04)
+    {
+        c6_env.Trigger();
+    }
+
+
+    //Prepare the audio block
+    for(size_t i = 0; i < size; i += 2)
+    {
+        c5.SetAmp(c5_env.Process());
+        c5_out = c5.Process();
+        d5.SetAmp(d5_env.Process());
+        d5_out = d5.Process();
+        e5.SetAmp(e5_env.Process());
+        e5_out = e5.Process();
+        f5.SetAmp(f5_env.Process());
+        f5_out = f5.Process();
+        g5.SetAmp(g5_env.Process());
+        g5_out = g5.Process();
+        a5.SetAmp(a5_env.Process());
+        a5_out = a5.Process();
+        b5.SetAmp(b5_env.Process());
+        b5_out = b5.Process();
+        c6.SetAmp(c6_env.Process());
+        c6_out = c6.Process();
+
+        lfo_out = lfo.Process();
+
+        sig = c5_out + d5_out + e5_out + f5_out 
+              + g5_out + a5_out + b5_out + c6_out;
+
+        sig *= lfo_out;
+        sig *= vol;
+        //Set the left and right outputs to the mixed signals
+        out[i]     = sig;
+        out[i + 1] = sig;
+    }
+}
+
+int main(void)
+{
+    // Configure and Initialize the Daisy Seed
+    // These are separate to allow reconfiguration of any of the internal
+    // components before initialization.
+    hw.Configure();
+    hw.Init();
+    hw.StartLog(DEBUG_PRINT);
+    hw.PrintLine("Hello from Daisy Seed!");
+    hw.SetAudioBlockSize(4);
+
+    //set up ADC on pin D16
+    const int adcChannels = 2;
+    AdcChannelConfig adcConfig[adcChannels];
+    adcConfig[0].InitSingle(hw.GetPin(15));
+    adcConfig[1].InitSingle(hw.GetPin(16));
+    hw.adc.Init(adcConfig, adcChannels);
+    hw.PrintLine("init ADC");
+    hw.adc.Start();
+
+    hw.PrintLine("started ADC");
+    //set up mux (for this program, just set it to the last input)
+    GPIO mux0, mux1, mux2, mux3;
+    mux0.Init(seed::D17, GPIO::Mode::OUTPUT);
+    mux1.Init(seed::D18, GPIO::Mode::OUTPUT);
+    mux2.Init(seed::D19, GPIO::Mode::OUTPUT);
+    mux3.Init(seed::D20, GPIO::Mode::OUTPUT);
+    hw.PrintLine("initialized mux pins");
+
+    mux0.Write(1);
+    mux1.Write(1);
+    mux2.Write(1);
+    mux3.Write(1);
+    hw.PrintLine("pulled mux pins high");
+
+    //hall_val = hw.adc.GetFloat(1);
+    //hw.PrintLine("ADC value from Hall sensor: %f", hall_val);
+
+    float samplerate = hw.AudioSampleRate();
+    // setup the configuration
+    I2CHandle::Config i2c_conf;
+    i2c_conf.periph = I2CHandle::Config::Peripheral::I2C_1;
+    i2c_conf.speed  = I2CHandle::Config::Speed::I2C_400KHZ;
+    i2c_conf.mode   = I2CHandle::Config::Mode::I2C_MASTER;
+    i2c_conf.pin_config.scl  = {DSY_GPIOB, 8};
+    i2c_conf.pin_config.sda  = {DSY_GPIOB, 9};
+    // initialise the peripheral
+    I2CHandle i2c;
+    i2c.Init(i2c_conf);
+    // now i2c points to the corresponding peripheral and can be used.
+
+    hw.PrintLine("Attempting to setup MPR121 now.");
+    bool result = init_MPR121(&i2c, MPR121_ADDR, false);
+    hw.PrintLine("Result: %s", (result ? "success" : "failed!"));
+
+#if DEBUG_PRINT == true
+    uint8_t mprdata = 0;
+    System::Delay(64);
+
+    mprdata = 0;
+    i2c.ReadDataAtAddress(MPR121_ADDR, AUTOCFG_0, 1, &mprdata, 1, MAX_I2C_WAIT);
+    hw.Print("Autoconfig 0 Reg: ");
+    hw.Print("%#X\r\n", mprdata);
+
+    System::Delay(64);
+
+    mprdata = 0;
+    i2c.ReadDataAtAddress(MPR121_ADDR, AUTOCFG_1, 1, &mprdata, 1, MAX_I2C_WAIT);
+    hw.Print("Autoconfig 1 Reg: ");
+    hw.Print("%#X\r\n", mprdata);
+
+    System::Delay(64);
+
+    mprdata = 0;
+    i2c.ReadDataAtAddress(MPR121_ADDR, ELECTRODE_CFG, 1, &mprdata, 1, MAX_I2C_WAIT);
+    hw.Print("Read from Electrode Enable Reg: ");
+    hw.Print("%#X\r\n", mprdata);
+
+#endif
+    //initialize oscillators
+    c5.Init(samplerate);
+    c5.SetWaveform(Oscillator::WAVE_TRI);
+    c5.SetAmp(1); 
+    c5.SetFreq(523.25);
+    
+    d5.Init(samplerate);
+    d5.SetWaveform(Oscillator::WAVE_TRI);
+    d5.SetAmp(1); 
+    d5.SetFreq(587.33);
+
+    e5.Init(samplerate);
+    e5.SetWaveform(Oscillator::WAVE_TRI);
+    e5.SetAmp(1); 
+    e5.SetFreq(659.26);
+
+    f5.Init(samplerate);
+    f5.SetWaveform(Oscillator::WAVE_TRI);
+    f5.SetAmp(1); 
+    f5.SetFreq(698.46);
+
+    g5.Init(samplerate);
+    g5.SetWaveform(Oscillator::WAVE_TRI);
+    g5.SetAmp(1); 
+    g5.SetFreq(783.99);
+
+    a5.Init(samplerate);
+    a5.SetWaveform(Oscillator::WAVE_TRI);
+    a5.SetAmp(1); 
+    a5.SetFreq(880.0);
+
+    b5.Init(samplerate);
+    b5.SetWaveform(Oscillator::WAVE_TRI);
+    b5.SetAmp(1); 
+    b5.SetFreq(987.77);
+
+    c6.Init(samplerate);
+    c6.SetWaveform(Oscillator::WAVE_TRI);
+    c6.SetAmp(1); 
+    c6.SetFreq(1046.5);
+
+    hw.PrintLine("initialized scale oscillators");
+
+    lfo.Init(samplerate);
+    lfo.SetWaveform(Oscillator::WAVE_SIN);
+    lfo.SetAmp(1);
+    lfo.SetFreq(10);
+
+    hw.PrintLine("initialized lfo");
+
+    //initialize envelopes
+    c5_env.Init(samplerate);
+    c5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    c5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    c5_env.SetMax(1);
+    c5_env.SetMin(0);
+
+    d5_env.Init(samplerate);
+    d5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    d5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    d5_env.SetMax(1);
+    d5_env.SetMin(0);
+
+    e5_env.Init(samplerate);
+    e5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    e5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    e5_env.SetMax(1);
+    e5_env.SetMin(0);
+
+    f5_env.Init(samplerate);
+    f5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    f5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    f5_env.SetMax(1);
+    f5_env.SetMin(0);
+
+    g5_env.Init(samplerate);
+    g5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    g5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    g5_env.SetMax(1);
+    g5_env.SetMin(0);
+
+    a5_env.Init(samplerate);
+    a5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    a5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    a5_env.SetMax(1);
+    a5_env.SetMin(0);
+
+    b5_env.Init(samplerate);
+    b5_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    b5_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    b5_env.SetMax(1);
+    b5_env.SetMin(0);
+
+    c6_env.Init(samplerate);
+    c6_env.SetTime(ADENV_SEG_ATTACK, ENV_ATK_TIME);
+    c6_env.SetTime(ADENV_SEG_DECAY, ENV_DECAY_TIME);
+    c6_env.SetMax(1);
+    c6_env.SetMin(0);
+
+    //wait 2 seconds for MPR121 to get baseline values
+    System::Delay(2000);
+    //Start calling the callback function
+    hw.StartAudio(AudioCallback);
+    hw.PrintLine("started Audio Callback");
+
+    // Loop forever
+    while(1)
+    {
+	hw.PrintLine("At top of While loop");
+   	hw.SetLed(readTouch(&i2c) > 0);  
+	//wait for touch
+	touch_hi = touch_lo = 0;
+	while(readTouch(&i2c) == 0)
+	{
+		System::Delay(64);
+	}
+	i2c.ReadDataAtAddress(MPR121_ADDR, TOUCH_STATUS_LO, 1, &touch_lo, 1, MAX_I2C_WAIT);
+    	hw.PrintLine("Read from TOUCH_REG_LO : 0x%X", touch_lo);
+	i2c.ReadDataAtAddress(MPR121_ADDR, TOUCH_STATUS_HI, 1, &touch_hi, 1, MAX_I2C_WAIT);
+    	hw.PrintLine("Read from TOUCH_REG_HI: 0x%X", touch_hi);
+	System::Delay(64);
+        //hall_val = hw.adc.GetFloat(0);
+        hw.PrintLine("ADC value from Hall sensor: %d",  hw.adc.Get(0));
+        hw.PrintLine("Simulated LFO frequency: %d", int(hall_val * 2.357 * 8));
+    }
+}
---
+++ src/final_jun_24/mpr121.h
@@ -0,0 +1,117 @@
+// MPR121 library
+#pragma once
+
+#include "daisy_seed.h"
+#include <stdio.h>
+#include <src/per/i2c.h>
+
+using daisy::I2CHandle;
+using daisy::DaisySeed;
+
+//maximum wait before returning from I2C transaction
+#ifndef MAX_I2C_WAIT
+#define MAX_I2C_WAIT 500
+#endif
+
+// address is 7 bit, so need to shift left 1
+#define MPR121_ADDR (0x5A << 1)
+// register addresses
+#define TOUCH_STATUS_LO 0x00
+#define TOUCH_STATUS_HI 0x01
+#define OOR_STATUS_LO 0x02
+#define OOR_STATUS_HI 0x03
+#define ELE0_BASELINE 0x1E
+#define ELE1_BASELINE 0x1F
+#define ELE2_BASELINE 0x20
+#define ELE3_BASELINE 0x21
+#define ELE4_BASELINE 0x22
+#define ELE5_BASELINE 0x23
+#define ELE6_BASELINE 0x24
+#define ELE7_BASELINE 0x25
+#define ELE8_BASELINE 0x26
+#define ELE9_BASELINE 0x27
+#define ELE10_BASELINE 0x28
+#define ELE11_BASELINE 0x29
+#define MHD_RISING 0x2B
+#define NHD_RISING_AMT 0x2C
+#define NCL_RISING 0x2D
+#define FDL_RISING 0x2E
+#define MHD_FALLING 0x2F
+#define NHD_FALLING_AMT 0x30
+#define NCL_FALLING 0x31
+#define FDL_FALLING 0x32
+#define NHD_AMT_TOUCHED 0x33
+#define NCL_TOUCHED 0x34
+#define FDL_TOUCHED 0x35
+#define E0TTH 0x41
+#define E0RTH 0x42
+#define E1TTH 0x43
+#define E1RTH 0x44
+#define E2TTH 0x45
+#define E2RTH 0x46
+#define E3TTH 0x47
+#define E3RTH 0x48
+#define E4TTH 0x49
+#define E4RTH 0x4A
+#define E5TTH 0x4B
+#define E5RTH 0x4C
+#define E6TTH 0x4D
+#define E6RTH 0x4E
+#define E7TTH 0x4F
+#define E7RTH 0x50
+#define E8TTH 0x51
+#define E8RTH 0x52
+#define E9TTH 0x53
+#define E9RTH 0x54
+#define E10TTH 0x55
+#define E10RTH 0x56
+#define E11TTH 0x57
+#define E11RTH 0x58
+#define DEBOUNCE_T_R 0x5B
+#define FILTER_CDC_CFG 0x5C
+#define FILTER_CDT_CFG 0x5D
+#define ELECTRODE_CFG 0x5E
+#define GPIO_ENA 0x77
+#define AUTOCFG_0 0x7B
+#define AUTOCFG_1 0x7C
+#define UPSIDE_LIMIT 0x7D
+#define LOWSIDE_LIMIT 0x7E
+#define TARGET_LEVEL 0x7F
+#define SOFT_RESET 0x80
+
+//config register bit offsets
+//0x5B - Debounce Register
+#define DR 0x04
+#define DT 0x00
+//0x5C - Filter/Global CDC Configuration Register
+#define FFI 0x06
+#define CDC 0x00
+//0x5D - Filter/Global CDT Configuration Register
+#define CDT 0x05
+#define SFI 0x03
+#define ESI 0x0
+//0x5E - Electrode Configuration Register
+#define ELECT_CL 0x06
+#define ELECT_ELEPROX 0x04
+#define ELECT_ELE_EN 0x00
+//0x7B - Auto Config Control Register 0
+#define AUTOCFG_ACE 0x00
+#define AUTOCFG_ARE 0x01
+#define AUTOCFG_BVA 0x02
+#define AUTOCFG_RETRY 0x04
+#define AUTOCFG_FFI 0x06
+//0x7C - Auto Config Control Register 0
+#define AUTOCFG_ACFIE 0x00
+#define AUTOCFG_ARFIE 0x01
+#define AUTOCFG_OORIE 0x02
+#define AUTOCFG_SCTS 0x07
+
+// Known good register settings taken from Adafruit MPR121 library
+// check out https://github.com/adafruit/Adafruit_MPR121/
+#define TOUCH_THRESHOLD 12
+#define RELEASE_THRESHOLD 6
+#define SOFT_RESET_MAGIC_NUMBER 0x63
+
+bool init_MPR121(I2CHandle* i2c, uint8_t addr = MPR121_ADDR, bool autoconfig = true);
+uint16_t readTouch(I2CHandle* i2c, uint8_t addr = MPR121_ADDR);
+bool setThresholds(I2CHandle* i2c, uint8_t touch, uint8_t release, uint8_t addr = MPR121_ADDR);
